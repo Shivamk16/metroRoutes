5f87d2aacbc8fb371f2a670451407e4933067b9d
Shivamk16 <16shivamk@gmail.com>
2021-09-09 16:26:00 +0530

    init

---
+++ src/test/java/com/metroDemo/springbootmavenmetrodemo/SpringbootMavenMetroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metroDemo.springbootmavenmetrodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMavenMetroDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
